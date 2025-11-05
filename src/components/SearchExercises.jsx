790282deb42144ff888f41beabe3ea9839c63e01
nimeduhansaka <nimedu.h.gbro@gmail.com>
2025-11-06 01:13:14 +0530

    Created fitness app project

---
+++ src/components/SearchExercises.jsx
@@ -0,0 +1,32 @@
+import React, {useState, useEffect} from 'react'
+import  { Box, Button, Stack, TextField, Typography } from '@mui/material'
+
+const SearchExercises = () => {
+  return (
+    <Stack alignItems="center" 
+           mt="37px" justifyContent="center" p="20px">
+
+            <Typography fontWeight={700} 
+                        sx={{ fontSize: { lg: '44px', xs: '30px' } }} 
+                        mb="50px" textAlign="center"
+           >
+              Awesome Exercises <br/>
+              You Should Know
+            </Typography>
+
+            <Box position="relative" mb="72px">
+              <TextField
+              sx={{input:{fontWeight:"700px"}}}
+              height="76px"
+              value=""
+              onChange={(e) => {}}
+              placeholder="Search Exercises" 
+              type="text"
+            />
+            </Box>
+
+    </Stack>
+  )
+}
+
+export default SearchExercises
